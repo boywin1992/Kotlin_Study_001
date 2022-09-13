082716d57548a83b5c9a0acd2937b52a54fd10c6
SWM_hsson <t1900604@partner.tmapmobility.com>
2022-09-13 10:56:45 +0900

    001

---
+++ app/src/main/java/com/study/kotlin001/study_001_abstract_class/AbstractJavaClass.java
@@ -0,0 +1,4 @@
+package com.study.kotlin001.study_001_abstract_class;
+
+public class AbstractJavaClass {
+}
---
+++ app/src/main/java/com/study/kotlin001/study_001_abstract_class/AbstractKotlinClass.kt
@@ -0,0 +1,11 @@
+package com.study.kotlin001.study_001_abstract_class
+
+abstract class AbstractKotlinClass { // 추상 함수를 포함하는 Class 앞에 abstract 를 지정해야 합니다.
+
+    fun myFunc001() {
+
+    }
+
+    abstract fun myFunc002() // 추상 함수는 선언부 앞에 abstract 키워드를 붙여야 합니다.
+
+}
